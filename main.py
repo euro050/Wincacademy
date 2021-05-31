566e207ec64a471aed9696d7ca3bfd7d44a7f0e3
euro050 <81748018+euro050@users.noreply.github.com>
2021-05-31 15:54:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,46 @@
-# Do not modify these lines
-__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
-__human_name__ = 'arguments'
-
-# Add your code after this line
-def greet(name, greeting = 'Hello, name!'):
-    greeting = greeting.replace('name', name)
-    return print(greeting)
-    
-
-greet('Bob')
-greet('Mark')
-
-def force(mass, body = 'earth'):
-    gravitylist = dict([
-        ('sun', 274),
-        ('jupiter',  24.9),
-        ('neptune',  11.2),
-        ('saturn',  10.4),
-        ('earth', 9.8),
-        ('uranus', 8.9),
-        ('venus', 8.9),
-        ('mars', 3.7),
-        ('mercury', 3.7),
-        ('moon', 1.6),
-        ('pluto', 0.6)
-    ])
-    print(mass * gravitylist[body])
+__winc_id__ = 'ae539110d03e49ea8738fd413ac44ba8'
+__human_name__ = 'files'
 
 
-force(18.2)
+import os
+import shutil
+import zipfile
 
 
+main_path = os.path.dirname(os.path.realpath(__file__))
+cache_path = str(main_path) + '\\cache'
 
-#def pull(mass1, mass2, distance):
-    
-G = 6.674 * (10**-11)
+#deel1
+def clean_cache():
+    os.chdir(main_path)
+    if os.path.exists(cache_path):
+        shutil.rmtree(cache_path)
 
-def pull(m1, m2, d):
-    print(G * (m1 * m2) / (d**2))
-    return
+    os.mkdir(cache_path)
 
+#deel2
+def cache_zip(source, target):
+    with zipfile.ZipFile(source, 'r') as zipObj:
+        zipObj.extractall(target)
 
-pull(100,200,0.5)
+#deel3
+def cached_files():
+    os.chdir(cache_path)
+    for root, dirs, files in os.walk('.'):
+        files_list = [os.path.abspath(name) for name in files]
 
+    return files_list
 
+#deel4
+def find_password(files_list):
+    for file in files_list:
+        with open(file) as f:
+            content = f.read().lower().splitlines()
+            for line in content:
+                if 'password' in line:
+                    password = line.split(': ')[1]
+                    os.chdir(main_path)  
+                    return password
 
-
-
-
-
+    print('password not found')
 
